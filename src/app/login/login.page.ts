9f5f2ec47746b775e4a651dfdc52e673089625d4
Gustavo Fernandes da Silva <gustavofernandes330@gmail.com>
2023-05-25 21:54:39 -0300

    Resolução de problema de merge01

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,15 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor(
-  ) { }
+  constructor() { }
 
-
-
+  public email: string = '';
+  public password: string = '';
   public showPassword: boolean = false;
+  public keepMeLoggedIn: boolean = false;
 
   ngOnInit() {
+    this.email = '';
+    this.password = '';
     this.showPassword = false;
+    this.keepMeLoggedIn = false;
   }
 
   togglePassword():void{this.showPassword === true ? this.showPassword = false : this.showPassword = true}
