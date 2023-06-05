262d9e4824a3c6cccb27ab850489cc490fef4c12
Gabriel Carvalho <gabrielpachucarvalho@gmail.com>
2023-06-05 20:56:57 -0300

    Página de dicas

---
+++ src/app/home/menu/dica/dica.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dica',
+  templateUrl: './dica.page.html',
+  styleUrls: ['./dica.page.scss'],
+})
+export class DicaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
