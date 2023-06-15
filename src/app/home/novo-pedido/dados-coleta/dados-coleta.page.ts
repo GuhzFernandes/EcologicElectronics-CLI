174acd6cc924d4eb1c419588843fba66590fa11c
Igor <igor_pessoa2011@outlook.com>
2023-06-15 01:39:41 -0300

    adição do viacep

--- src/app/home/novo-pedido/dados-coleta/dados-coleta.page.ts
+++ src/app/home/novo-pedido/dados-coleta/dados-coleta.page.ts
@@ -13,10 +13,10 @@ export class DadosColetaPage implements OnInit {
   formatarCVV(cvv: string): string {
     // Remove todos os caracteres que não sejam dígitos
     const numeros = cvv.replace(/\D/g, '');
-  
+
     // Limita o valor para no máximo 3 dígitos
     const cvvFormatado = numeros.slice(0, 3);
-  
+
     return cvvFormatado;
   }
 
@@ -43,33 +43,56 @@ export class DadosColetaPage implements OnInit {
 
     return validadeFormatado;
   }
+  //Definindo o cep pelo via cep
+  public cep: string = '';
+  public logradouro = '';
+  public bairro = '';
+  public complemento = '';
 
   //Definindo o segmento padrão para ser exibido
   public segmento: string = 'pix';
   public tamanho: string = '';
 
-  public idUsuario?: number; 
+  public idUsuario?: number;
   public statusPagamento: string = 'Aguard. pgto';
   public statusPedido: string = 'Aguard. pgto';
 
-  
-  public namecard:string = '';
+
+  public namecard: string = '';
   public numcard: string = '';
   public validcard: string = '';
   public cvvcard: string = '';
 
-  constructor(private router: Router,private loginService: LoginService,private pedidoService: PedidoService) {
+  constructor(private router: Router, private loginService: LoginService, private pedidoService: PedidoService) {
   }
 
   async ngOnInit() {
-    await this.loginService.getUser().then( (user) => {
+    await this.loginService.getUser().then((user) => {
       this.idUsuario = user.id;
     });
   }
 
-  public confirmarPedido(){
-    this.pedidoService.novoPedido(this.segmento,this.statusPagamento,this.statusPedido,this.tamanho,this.idUsuario);
+  public confirmarPedido() {
+    this.pedidoService.novoPedido(this.segmento, this.statusPagamento, this.statusPedido, this.tamanho, this.idUsuario);
     this.router.navigate(["/home/novo-pedido/pedido-confirmado"]);
   }
 
+  buscarCEP() {
+    if (this.cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${this.cep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+          this.logradouro = data.logradouro;
+          this.bairro = data.bairro;
+          this.complemento = data.complemento;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    } else {
+      this.logradouro = '';
+      this.bairro = '';
+      this.complemento = '';
+    }
+  }
 }
